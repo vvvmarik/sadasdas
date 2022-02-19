1c52cdabe5b97d73fd2b593528b5d2afb3ebe8cc
vitalik <vitalik.94.07@gmail.com>
2022-02-19 19:25:40 +0200

    commit me

---
+++ app.js
@@ -0,0 +1,57 @@
+//1 Переменные
+//camelCase = firstName
+//const firstName = 'Vitalik' //string
+//const lastName = 'Marchuk'
+//const age = 27 
+//const isProgrammer = true //boolean
+
+
+
+//const withNum5 = '5'
+//const if = 'fgsfg'//err
+//const 5withNum = '5'//err
+
+// 2 Мутирование
+//console.log('Имя человека: ' + firstName + ',а возвраст человека ' + age)
+//alert('Имя человека: ' + firstName + ',а возвраст человека ' + age)
+//const lastName = prompt('Введите фамилию')
+//alert(firstName + ' ' + lastName )
+
+
+// 3 Операторы
+//let currentYear = 2022
+//const birthYear = 1994
+
+//const age = (currentYear + birthYear)
+//const a = 10
+//const b = 5
+
+//let c = 32
+//c = c + a
+//c += a
+//c -= a
+//c *= a
+//c /= a
+
+//console.log(a - b)
+//console.log(a + b)
+//console.log(a * b)
+//console.log(a / b)
+//console.log(++currentYear)
+//console.log(--currentYear)
+//console.log(c)
+
+
+// 4 Типы данных
+
+// const isProgrammer = true
+// const name = 'Vitalik'
+// const age = 27
+// let x
+// console.log(typeof isProgrammer)
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof x)
+
+// 5 Приоритет операторов
+
